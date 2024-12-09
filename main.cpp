5d07769474f35339321347002eb479442c22aa98
MeanBeanie <itsawildveg@gmail.com>
2024-12-08 16:58:02 -0800

    updated to be able to be restarted

--- main.cpp
+++ main.cpp
@@ -35,6 +35,8 @@ int main(){
 	int minutes = 0;
 	int seconds = 0;
 
+	bool paused = false;
+
 	int verticalOffset = (LINES / 2) - 2;
 	int horizOffset = (COLS / 2 ) - 28;
 	
@@ -42,28 +44,44 @@ int main(){
 
 	bool running = true;
 	while(running){
-		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-		seconds++;
-		if(seconds % 15 == 0){
-			int now = time(NULL);
-			seconds = now - start;
-			minutes = seconds / 60;
-			seconds -= ((seconds / 60) * 60);
-			hours = minutes / 60;
-			minutes -= ((minutes / 60) * 60);
-		}
-		
-		formatted = (hours < 10 ? "0" : "") + std::to_string(hours) + ":" + (minutes < 10 ? "0" : "") + std::to_string(minutes) + ":" + (seconds < 10 ? "0" : "") + std::to_string(seconds);
+		if(!paused){
+			std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+			seconds++;
+			if(seconds % 15 == 0){
+				int now = time(NULL);
+				seconds = now - start;
+				minutes = seconds / 60;
+				seconds -= ((seconds / 60) * 60);
+				hours = minutes / 60;
+				minutes -= ((minutes / 60) * 60);
+			}
+			
+			formatted = (hours < 10 ? "0" : "") + std::to_string(hours) + ":" + (minutes < 10 ? "0" : "") + std::to_string(minutes) + ":" + (seconds < 10 ? "0" : "") + std::to_string(seconds);
 
-		for(int i = 0; i < formatted.size(); i++){
-			for(int j = 0; j < numberMap.at(formatted[i]).size(); j++){
-				mvprintw(j+verticalOffset, (i*7)+horizOffset, "%s", numberMap.at(formatted[i]).at(j).c_str());
+			for(int i = 0; i < formatted.size(); i++){
+				for(int j = 0; j < numberMap.at(formatted[i]).size(); j++){
+					mvprintw(j+verticalOffset, (i*7)+horizOffset, "%s", numberMap.at(formatted[i]).at(j).c_str());
+				}
 			}
 		}
 
 		int input = getch();
 		if(input == 'q'){
 			running = false;
+		}
+		else if(input == 'p'){
+			paused = !paused;
+			if(paused){
+				mvprintw(0,0,"PAUSED");
+			}
+			else{
+				mvprintw(0,0,"      ");
+			}
+		}
+		else if(input == 'r'){
+			hours = 0;
+			minutes = 0;
+			seconds = 0;
 		}
 	}
 
